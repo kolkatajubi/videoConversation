de9858c5e78954af592c8011696b11c942e7c6a8
unknown <kolkatachamps@jubi.ai>
2019-11-11 20:19:32 +0530

    breaking code 3

--- assets/files/moduleRunner.js
+++ assets/files/moduleRunner.js
@@ -415,10 +415,12 @@ function getNextStageData(nextStage) {
 function videoDisplay(videoData) {
   // console.log("videoDisplay...");
   // console.log(videoData);
-  $("#myVideo").empty();
-  $("#myVideo").append(videoData
-    // "<source id='start' type='video/mp4' src='" + videoData + "' />"
-  );
+  $(".display").empty();
+  $(".display").append(videoData);
+  // $("#myVideo").empty();
+  // $("#myVideo").append(
+  //   "<source id='start' type='video/mp4' src='" + videoData + "' />"
+  // );
   // console.log("<source id='start' type='video/mp4' src='" + videoData + "' />");
   // $("#myVideo").attr("poster", "");
   var video = document.getElementById("myVideo");
