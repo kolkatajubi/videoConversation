3fd982376c73d951870ab87f067ac2b60fa4b290
Arkadeep <arkadeepbag3390@gmail.com>
2019-11-21 16:28:47 +0530

    polling

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var Schema = mongoose.Schema;
   (UserClick = mongoose.model("click", userSchema));
 module.exports = UserClick;*/
 var arr = [];
-console.log(arr);
+console.log("array==" + arr);
 app.use("/static", express.static(path.join(__dirname, "assets")));
 // app.use(express.static(__dirname + "/css"));
 // app.use(express.static(__dirname + "/videos"));
