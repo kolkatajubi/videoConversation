32189f9f04a2144f47fa869f1cedabb95db081a2
Arkadeep <arkadeepbag3390@gmail.com>
2019-11-21 17:05:18 +0530

    polling

--- assets/files/moduleRunner.js
+++ assets/files/moduleRunner.js
@@ -324,9 +324,7 @@ $(document).ready(() => {
       console.log(i + ":" + totalclicks[i]);
     }
     localStorage.setItem("tempclick", JSON.stringify(totalclicks));
-    let getclick = JSON.parse(localStorage.getItem(tempclick));
-    console.log("temporary click");
-    console.log(getclick);
+    console.log(localStorage.getItem("tempcick"));
     if (type != "url") {
       // console.log("after button clicked count will be");
 
