64adcbb4e72c60c99ae5c7cbe5d279ed5eb9cd1a
Arkadeep <arkadeepbag3390@gmail.com>
2019-11-25 15:59:26 +0530

    polling

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.post("/savedata", function(req, res) {
   };
 
   var data = new UserClick(item);
-  data.UpdateOne(function(err, data) {
+  data.updateOne(function(err, data) {
     if (err) {
       console.log(err);
       console.log("error while saving data");
