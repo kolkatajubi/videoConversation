7ef91164f7c14f6d2b1bb0a18d49ad2a1e7677cf
Arkadeep <arkadeepbag3390@gmail.com>
2019-11-26 12:26:16 +0530

    polling

--- assets/files/moduleRunner.js
+++ assets/files/moduleRunner.js
@@ -391,15 +391,13 @@ $(document).ready(() => {
           for (j in totalclicks) {
             if (currentData.next.data[i].data == j) {
               if (number_of_button_created_perStage_count > 1 && sum > 0) {
-                /*display =
+                console.log("button display");
+                display =
                   display +
                   currentData.next.data[i].text +
                   " " +
                   console.log(Math.floor((totalclicks[j] / sum) * 100)) +
-                  "%";*/
-                document.getElementsByClassName(
-                  "button-list"
-                ).innerHTML = Math.floor((totalclicks[j] / sum) * 100);
+                  "%";
               } else {
                 display =
                   display +
