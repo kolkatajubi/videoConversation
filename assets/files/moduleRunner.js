cfbfce7708cdc2ffa90cfad98938972cbdfd06ec
Arkadeep <arkadeepbag3390@gmail.com>
2019-11-27 17:47:14 +0530

    polling

--- assets/files/moduleRunner.js
+++ assets/files/moduleRunner.js
@@ -457,7 +457,7 @@ $(document).ready(() => {
       //}
       //});
       getSiblings(button).forEach(el => {
-        el.style.opacity = "0.1";
+        el.style.opacity = "0.5";
         el.classList.toggle("inactive");
         el.addEventListener("animationend", () => {
           el.classList.remove("inactive");
