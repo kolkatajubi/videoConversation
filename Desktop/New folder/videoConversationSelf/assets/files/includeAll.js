021135ee5af00b58d90c7f8bf41f808bd4572cbd
kolkatajubi <kolkatachamps@jubi.ai>
2019-12-18 20:10:24 +0530

    clean

--- Desktop/New folder/videoConversationSelf/assets/files/includeAll.js
+++
@@ -1,7 +0,0 @@
-document.write(`
-<link href="https://fonts.googleapis.com/css?family=Muli:700&display=swap" rel="stylesheet" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://pixie.jubi.ai/videoConversationSelf/static/files/moduleRunner.js"></script>
-<link rel="stylesheet" href="https://pixie.jubi.ai/videoConversationSelf/static/css/button.css" />
-<link rel="stylesheet" href="https://pixie.jubi.ai/videoConversationSelf/static/css/text.css" />
-`);
